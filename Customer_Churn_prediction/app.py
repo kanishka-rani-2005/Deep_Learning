a49cf237255bf240b8fd33f393e33e617f4dc0f0
Kanishka Rani <kanishka22043@gmail.com>
2025-04-02 17:52:06 +0530

    Update app.py

--- Customer_Churn_prediction/app.py
+++ Customer_Churn_prediction/app.py
@@ -6,21 +6,19 @@ import pickle;
 from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder;
 
 from tensorflow import keras
-import os
 
-# Load the model
-model = tf.keras.models.load_model(os.path.join(os.getcwd(), 'model.h5'))
+model = tf.keras.models.load_model('model.h5')
 
-# Load encoders and scaler
-import pickle
-with open(os.path.join(os.getcwd(), 'label_encoder_gender.pkl'), 'rb') as f:
+
+with open ('label_encoder_gender.pkl','rb') as f:
     le_gender = pickle.load(f)
 
-with open(os.path.join(os.getcwd(), 'onehot_encoder_geo.pkl'), 'rb') as f:
+with open ('onehot_encoder_geo.pkl','rb') as f:
     ohe_geo = pickle.load(f)
 
-with open(os.path.join(os.getcwd(), 'scaler.pkl'), 'rb') as f:
+with open('scaler.pkl','rb') as f:
     scaler = pickle.load(f)
+
 
 
 ## Streamlit app
